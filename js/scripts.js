3cc6d3d7ca8c288c13a18f78aa7f4578a061c63b
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-02 13:23:55 -0700

    added deposit and withdrawal functions
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+//Business Logic ------
+function bankAccount() {
+  this.account = {};
+}
+
+
+
+
+
+//UI Logic ------
 $(document).ready(function() {
   $("#mainForm").submit(function(event) {
     event.preventDefault();
